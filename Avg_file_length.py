c979033c1303907066981675d5ce0d1b6f14de39
Anna1Lin <annalincc@gmail.com>
2021-02-07 00:50:28 +0800

    To filter good and bad comments number in reviews.txt file

--- Avg_file_length.py
+++ Avg_file_length.py
@@ -15,10 +15,30 @@ for d in data:
 print("The avg length is ", sum_len/len(data))
 
 new = []
-
 for d in data:
     if len(d) < 100:
         new.append(d)
 print("Filter there are", len(new), "comments under 100 length")
 print(new[0])
+print("---------------------")
 print(new[1])
+
+good = []
+for d in data:
+    if "good" in d:
+        good.append(d)
+print(len(good))
+print("There are", len(good),"good comments in reviews.txt")
+print(good[0])
+print("---------------------")
+print(good[1])
+
+bad = []
+for d in data:
+    if "bad" in d:
+        bad.append(d)
+print("There are", len(bad),"bad comments in reviews.txt")
+print()
+print(bad[0])
+print("---------------------")
+print(bad[1])
