d0d186727091e6137b56ae082d725caedd9964eb
Anna1Lin <annalincc@gmail.com>
2021-02-06 23:31:30 +0800

    To open a file and avg the file's length.

---
+++ Avg_file_length.py
@@ -0,0 +1,14 @@
+data = []
+count = 0
+with open("reviews.txt", "r") as f:
+    for line in f:
+        data.append((line))
+        count += 1
+        if count % 1000 == 0:
+            print(len(data))
+print("Finish read, there are: ", len(data), "data in file")
+
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print("The avg length is ", sum_len/len(data) )
