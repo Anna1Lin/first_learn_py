9d63b762d23987c761e948ba9a957e5d24fdda57
Anna1Lin <annalincc@gmail.com>
2021-02-07 00:01:47 +0800

    To filter the length under 100.

--- Avg_file_length.py
+++ Avg_file_length.py
@@ -13,3 +13,12 @@ sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
 print("The avg length is ", sum_len/len(data))
+
+new = []
+
+for d in data:
+    if len(d) < 100:
+        new.append(d)
+print("Filter there are", len(new), "comments under 100 length")
+print(new[0])
+print(new[1])
